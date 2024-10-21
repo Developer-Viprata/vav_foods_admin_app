5a694021b05f889b508a969c209752696848821f
Sai-Harshith-315 <saiharshith.vanguard@gmail.com>
2024-10-21 09:35:40 +0530

    updated code
    modified the add_categories_screen file in that added the contrller clear method to clear the text in the textFromFields.

--- lib/Presentation/Admin/screens/category_management/add_categories_screen.dart
+++ lib/Presentation/Admin/screens/category_management/add_categories_screen.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: avoid_print
 
 import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -26,198 +25,6 @@ class _AddCategoriesScreenState extends State<AddCategoriesScreen> {
       Get.find<AllCategoriesController>();
   final _formKey = GlobalKey<FormState>();
 
-/* 
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: MyAppBar(
-          title: "Add Category Screen",
-        ),
-        body: Center(
-          child: SingleChildScrollView(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                maxWidth: Responsive.isDesktop(context) ||
-                        Responsive.isDesktopLarge(context)
-                    ? MediaQuery.of(context).size.width * .4
-                    : Responsive.isTablet(context)
-                        ? MediaQuery.of(context).size.width * .7
-                        : Responsive.isMobile(context)
-                            ? MediaQuery.of(context).size.width * 1
-                            : MediaQuery.of(context).size.width * .9,
-              ),
-              child: Column(
-                children: [
-                  //select images
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        MyText(
-                          text: 'Select Images',
-                        ),
-                        ElevatedButton(
-                          onPressed: () async {
-                            // Trigger image selection
-                            await allCategoriesController.pickImages('gallery');
-                          },
-                          child: MyText(text: 'Select Images'),
-                        ),
-                      ],
-                    ),
-                  ),
-                  //display selected images
-                  Obx(
-                    () {
-                      return allCategoriesController.selectedImages.isNotEmpty
-                          ? Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 10.0),
-                              child: SizedBox(
-                                height: 300,
-                                width: 500,
-                                child: GridView.builder(
-                                  itemCount: allCategoriesController
-                                      .selectedImages.length,
-                                  physics: const BouncingScrollPhysics(),
-                                  gridDelegate:
-                                      const SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 2,
-                                    mainAxisSpacing: 20,
-                                    crossAxisSpacing: 20,
-                                  ),
-                                  itemBuilder:
-                                      (BuildContext context, int index) {
-                                    final isValidIndex = kIsWeb
-                                        ? index <
-                                            allCategoriesController
-                                                .webImageBytes.length
-                                        : true;
-                                    final imageFile = allCategoriesController
-                                        .selectedImages[index];
-                                    return Stack(
-                                      children: [
-                                        isValidIndex
-                                            ? (kIsWeb
-                                                ? Image.memory(
-                                                    allCategoriesController
-                                                        .webImageBytes[index],
-                                                    fit: BoxFit.cover,
-                                                    height: 500,
-                                                    width: 500,
-                                                  )
-                                                : Image.file(
-                                                    File(imageFile.path),
-                                                    fit: BoxFit.cover,
-                                                    height: 500,
-                                                    width: 500,
-                                                  ))
-                                            : Container(
-                                                color: Colors.grey[200],
-                                                child: const Center(
-                                                  child: Text(
-                                                      "Image Not Available"),
-                                                ),
-                                              ),
-                                        Positioned(
-                                          right: 10,
-                                          top: 0,
-                                          child: InkWell(
-                                            onTap: () {
-                                              allCategoriesController
-                                                  .removeImage(index);
-                                            },
-                                            child: CircleAvatar(
-                                              backgroundColor:
-                                                  AppColors.secondary,
-                                              child: Icon(
-                                                Icons.close,
-                                                color: AppColors.background,
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                      ],
-                                    );
-                                  },
-                                ),
-                              ),
-                            )
-                          : const SizedBox.shrink();
-                    },
-                  ),
-
-                  const SizedBox(
-                    height: 25,
-                  ),
-
-                  //category name and description form
-                  Form(
-                      key: _formKey,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                        child: Column(
-                          children: [
-                            //category name
-                            CustomTextFormField(
-                              hintText: 'Category Name',
-                              controller: allCategoriesController
-                                  .categoryNameController,
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Please enter Category Name';
-                                }
-                                return null;
-                              },
-                            ),
-                            const SizedBox(height: 20),
-                            //category description
-                            CustomTextFormField(
-                              controller: allCategoriesController
-                                  .categoryDescriptionController,
-                              hintText: 'Category Description',
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Please enter Category Description';
-                                }
-                                return null;
-                              },
-                            ),
-                            const SizedBox(height: 20),
-                            //add category button
-                            Obx(
-                              () => allCategoriesController.isLoading.value
-                                  ? const CupertinoActivityIndicator()
-                                  : MyButton(
-                                      onPressed: () async {
-                                        if (_formKey.currentState!.validate()) {
-                                          // Upload images before adding the category
-                                          await allCategoriesController
-                                              .uploadSelectedImages();
-                                          // Add category with the uploaded image URL
-                                          await allCategoriesController
-                                              .addCategory();
-                                        }
-                                      },
-                                      text: 'Add Category',
-                                    ),
-                            ),
-                          ],
-                        ),
-                      ))
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
- */
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -405,6 +212,12 @@ class _AddCategoriesScreenState extends State<AddCategoriesScreen> {
                                         allCategoriesController
                                             .isLoading.value = false;
                                       }
+                                      allCategoriesController
+                                          .categoryNameController
+                                          .clear();
+                                      allCategoriesController
+                                          .categoryDescriptionController
+                                          .clear();
                                     },
                                     text: 'Add Category',
                                   ),
